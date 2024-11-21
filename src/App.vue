39ae8d10926d1e8b7fb1948e6be604843a42ae9d
Ilya Konovalov <g2g@yandex.ru>
2024-11-21 14:54:19 +0300

    fixes: 2024

--- src/App.vue
+++ src/App.vue
@@ -5,26 +5,36 @@
         <div  v-for="i in selectedItems" :key="`items-${i}`" class="h-16 bg-gray-50 px-4 py-5 mr-6 mb-6">
           <dt class="text-sm font-medium text-gray-500">{{ i.name }}</dt>
         </div>
-        <p class="flex basis-full text-sm text-gray-500"> Selected: {{ selectedItemsCount }}</p>
+        <p class="flex basis-full text-sm text-gray-500"> Selected: {{ selectedItemsCount }} / {{ selectedItemsLeftCount }}</p>
       </div>
       <div  class="flex flex-wrap overflow-hidden bg-white shadow sm:rounded-lg p-8">
         <dt v-if="selectedItemRight" class="text-sm font-medium text-gray-500">{{ selectedItemRight.name }}</dt>
       </div>
       <div class="overflow-hidden bg-white shadow sm:rounded-lg flex flex-wrap flex-col p-8">
-        <RadioGroup v-model="selectedItemLeft">
-          <RadioGroupLabel class="sr-only">Choose left item </RadioGroupLabel>
-          <div class="grid grid-cols-4 grid-rows-2 gap-4">
-            <RadioGroupOption as="template" v-for="item in itemsLeft" :key="item.id" :value="item" v-slot="{ active, checked }">
-              <div :class="[active ? 'ring-2 ring-indigo-500' : '', 'bg-white shadow-sm text-gray-900 cursor-pointer group relative border rounded-md py-3 px-4 flex items-center justify-center text-sm font-medium uppercase hover:bg-gray-50 focus:outline-none sm:flex-1']">
-                <RadioGroupLabel as="span">{{ item.name }}</RadioGroupLabel>
-                <span :class="[active ? 'border' : 'border-2', checked ? 'border-indigo-500' : 'border-transparent', 'pointer-events-none absolute -inset-px rounded-md']" aria-hidden="true" />
-              </div>
-            </RadioGroupOption>
+        <h3 class="text-lg font-semibold mb-4">Выберите элементы:</h3>
+        <div class="flex flex-wrap gap-4">
+          <div
+            v-for="option in itemsLeft"
+            :key="option.id"
+            class="flex items-center cursor-pointer border-2 rounded-lg p-2 hover:cursor-pointer"
+            :class="{
+              'border-indigo-500': selectedItemLeft.some((obj:Item) => obj.id === option.id),
+              'border-gray-300': !selectedItemLeft.some((obj:Item) => obj.id === option.id)
+            }"
+          >
+            <input
+              type="checkbox"
+              :id="`${option.id}`"
+              :value="option"
+              v-model="selectedItemLeft"
+              class="hidden"
+            />
+            <label :for="`${option.id}`" class="ml-2 text-gray-700 hover:cursor-pointer">{{ option.name }}</label>
           </div>
-        </RadioGroup>
+        </div>
         <button
           type="submit"
-          :class="[selectedItemLeft ? 'bg-indigo-600' : 'pointer-events-none bg-gray-50 text-gray-200 cursor-not-allowed', 'mt-6 w-full h-12 items-center justify-center rounded-md border border-transparent py-3 px-8 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2']"
+          :class="[selectedItemLeft.length ? 'bg-indigo-600' : 'pointer-events-none bg-gray-50 text-gray-200 cursor-not-allowed', 'mt-6 w-full h-12 items-center justify-center rounded-md border border-transparent py-3 px-8 text-base font-medium text-white hover:bg-indigo-700']"
           @click="chooseItemLeft"
           >
             Add to bag
@@ -50,27 +60,36 @@
 <script setup lang="ts">
 import {computed, ref} from "vue";
 import {RadioGroupLabel, RadioGroup, RadioGroupOption} from "@headlessui/vue";
+import itemsLeft from "../data/itemsLeft.json"
+import itemsRight from "../data/itemsRight.json"
 
 interface Item {
   id: number;
   name: string;
 }
 
-import itemsLeft from "../data/itemsLeft.json"
-import itemsRight from "../data/itemsRight.json"
-
 let selectedItems = ref<Item[]>([]);
-let selectedItemLeft = ref<Item | null>(null)
-let selectedItemRight = ref<Item | null>(null)
+let selectedItemLeft = ref<Item[]>([])
+let selectedItemRight = ref<Item>()
 
-let selectedItemsCount = computed<number | null>(() => {
+let selectedItemsLeftCount = computed<number>(() => {
+  return itemsLeft.length
+})
+
+let selectedItemsCount = computed<number>(() => {
   return selectedItems.value.length
 })
 
 function chooseItemLeft():void {
-  if(selectedItemLeft.value !== null){
-    selectedItems.value.push(selectedItemLeft.value)
-    selectedItemLeft.value = null
+  if(selectedItemLeft.value.length > 0){
+    selectedItemLeft.value.forEach(item => {
+      let indexToRemove = itemsLeft.findIndex(obj => obj.id === item.id);
+      if (indexToRemove !== -1) {
+        itemsLeft.splice(indexToRemove, 1);
+        selectedItems.value.push(item)
+      }
+    })
+    selectedItemLeft.value = []
   }
 }
 </script>
